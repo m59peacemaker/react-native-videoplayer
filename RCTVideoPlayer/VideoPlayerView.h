00e922ef756ef6ba3c7374ad5678be38b3b1f9c5
Johnny Hauser <johnnyhauser@gmail.com>
2017-05-02 15:19:29 -0500

    updated import paths for newer React-Native version

--- RCTVideoPlayer/VideoPlayerView.h
+++ RCTVideoPlayer/VideoPlayerView.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
-#import <RCTView.h>
+#import <React/RCTView.h>
 
 @interface VideoPlayerView : UIView
 
--- RCTVideoPlayer/VideoPlayerView.m
+++ RCTVideoPlayer/VideoPlayerView.m
@@ -1,5 +1,5 @@
 #import "VideoPlayerView.h"
-#import "RCTRootView.h"
+#import "React/RCTRootView.h"
 
 @interface VideoPlayerView ()
 
